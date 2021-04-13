c646b6c492cab7760c67af058cbd2e6270ab3b0d
Stam743 <spencertam@rocketmail.com>
2021-04-12 19:53:27 -0600

    Fixed button layout for GrabberChatView and TaskInfo, as well as populated myTasks

--- TaskGrab/Pages/MyTasks.xaml.cs
+++ TaskGrab/Pages/MyTasks.xaml.cs
@@ -14,6 +14,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using TaskGrab.Navigation;
+using TaskGrab.Data;
+using TaskGrab.Controls;
+
 
 namespace TaskGrab.Pages
 {
@@ -24,7 +27,9 @@ namespace TaskGrab.Pages
     {
         private MainWindow main;
         private History history;
-        private QueryString query_string;
+        private QueryString query_string; 
+        TaskGrabContext posted = new();
+        string account = "Sharif";
         public MyTasks()
         {
             InitializeComponent();
@@ -39,17 +44,56 @@ namespace TaskGrab.Pages
 
             query_string = QueryString.Parse(request_url.Substring(query_start + 1));
         }
+        private void OnMessageClick(object sender, RoutedEventArgs e)
+        {
+            TaskControl task = (TaskControl)sender;
+            ((MainWindow)Application.Current.MainWindow).GetHistory().GoTo("Pages/GrabberChatView.xaml?id=" + task.Id + "&view=chat");
+        }
+
+        private void OnTaskClick(object sender, RoutedEventArgs e)
+        {
+            TaskControl task = (TaskControl)sender;
+            ((MainWindow)Application.Current.MainWindow).GetHistory().GoTo("Pages/TaskInfo.xaml?id=" + task.Id + "&view=info");
+        }
 
         private void postedButton_Click(object sender, RoutedEventArgs e)
         {
             postedButton.Foreground = new SolidColorBrush(Colors.Yellow);
             savedButton.Foreground = new SolidColorBrush(Colors.DarkBlue);
+            account = "Sharif";
+            List<TaskControl> tasks = new List<TaskControl>();
+
+            foreach (Data.Task task in posted.Tasks.Take(20))
+            {
+                if (task.poster == account)
+                {
+                    tasks.Add(new TaskControl(task));
+                    tasks.Last().MessageClick += OnMessageClick;
+                    tasks.Last().TaskClick += OnTaskClick;
+                }
+
+            }
+            TasksHolder.ItemsSource = tasks;
         }
 
         private void savedButton_Click(object sender, RoutedEventArgs e)
         {
             savedButton.Foreground = new SolidColorBrush(Colors.Yellow);
             postedButton.Foreground = new SolidColorBrush(Colors.DarkBlue);
+            account = "Dotty";
+            List<TaskControl> tasks = new List<TaskControl>();
+
+            foreach (Data.Task task in posted.Tasks.Take(20))
+            {
+                if (task.poster == account)
+                {
+                    tasks.Add(new TaskControl(task));
+                    tasks.Last().MessageClick += OnMessageClick;
+                    tasks.Last().TaskClick += OnTaskClick;
+                }
+
+            }
+            TasksHolder.ItemsSource = tasks;
         }
 
         private void signOutButton_Click(object sender, RoutedEventArgs e)
